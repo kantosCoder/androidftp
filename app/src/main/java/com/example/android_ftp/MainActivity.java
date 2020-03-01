d5e8dbf5ecfbad00752c2f274277a13f861355b1
kantosCoder <56650488+kantosCoder@users.noreply.github.com>
2020-03-01 21:18:28 +0100

    Formando

--- app/src/main/java/com/example/android_ftp/MainActivity.java
+++ app/src/main/java/com/example/android_ftp/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.SocketException;
+
 import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -26,11 +29,34 @@ import org.apache.commons.net.util.TrustManagerUtils;
 public class MainActivity extends AppCompatActivity {
 
     FTPClient client = new FTPClient();
+    private String sFTP = "";
+    private String sPort ="";
+    private String sUser = "";
+    private String sPassword = "";
+    private TextView ip;
+    private TextView port;
+    private TextView user;
+    private TextView pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ip = findViewById(R.id.ipfield);
+        port = findViewById(R.id.portfield);
+        user = findViewById(R.id.userfield);
+        pass = findViewById(R.id.passfield);
+    }
 
+    protected void checkserver(){
+        //obtener valores de campos
+        try {
+            client.connect(sFTP+":"+sPort);
+            boolean login = client.login(sUser,sPassword);
+            //como es valido, pasar a la siguiente pantalla con un intent
+        }
+        catch (Exception e) {
+            //mensaje de error al conectar toast
+        }
     }
 
 }
